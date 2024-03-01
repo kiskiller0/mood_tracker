971d8a6e86d50be406441ca7b9a4fc8389e2d555
Okba Boularaoui <okba2fb@gmail.com>
2024-03-01 15:08:58 +0300

    quick fixes

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -6,6 +6,9 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
+      <div className="content">
+        <p>No moods to display at the moment, login or create a new account first.</p>
+      </div>
     </div>
   );
 }
